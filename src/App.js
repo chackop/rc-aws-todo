426d4871b40daee3df2be5dbbfbbb3e535dbde99
Chacko <chackop@gmail.com>
2020-08-11 23:34:42 +0100

    created and published app with amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Auth from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Auth.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This a AWS Test App</p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
