84174761e1caf7046b286808ef5b769e8ea1b4f8
Chacko <chackop@gmail.com>
2020-08-12 00:46:54 +0100

    added basic mutations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Auth, API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
 import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations';
 
 Auth.configure(awsconfig);
 API.configure(awsconfig);
@@ -21,6 +22,15 @@ function App() {
     })
   );
   console.log(oneTodo);
+
+  Auth.currentAuthenticatedUser({
+    bypassCache: false,
+  })
+
+  const todo = { name: 'app', description: 'new todo' };
+  const newTodo = API.graphql(
+    graphqlOperation(mutations.createTodo, { input: todo })
+  );
 
   return (
     <div className="App">
